0b1cc888f0bbac84736fd61bbc55c6afe6f6a0f5
gabrieljimenez05 <32081034+gabrieljimenez05@users.noreply.github.com>
2020-10-22 21:54:01 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var images = {
 	man: {
 		heading: 'Man Image',
 		description: 'Image of a man.',
-		path: 'img/man.jpg'
+		path: 'images/man.jpg'
 	},
 	wizard: {
 		heading: 'Wizard Image',
 		description: 'Image of a wizard.',
-		path: 'img/wizard.jpg'
+		path: 'images/wizard.jpg'
 	},
 	beast: {
 		heading: 'Beast Image',
 		description: 'Image of a beast.',
-		path: 'img/beast.jpg'
+		path: 'images/beast.jpg'
 	}
 };
